7b8aff3e21d7412f2cf1bfe13ca0057584816864
Leo Nistor <leo.nistor@gmail.com>
2017-06-20 09:55:34 +0300

    templates, create function

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 
 const prog = require('caporal')
+const createCmd = require('./lib/create')
 
-prog.version('1.0.0')
+prog
+  .version('1.0.0')
   .command('create', 'Create a new application')
   .argument('<template>', 'Template to use')
   .option('--variant <variant>', 'Which <variant> of the template is going to be created')
-  .action((args, options, logger) => {
-    console.log({
-      args,
-      options
-    })
-  })
+  .action(createCmd)
 
 prog.parse(process.argv)
